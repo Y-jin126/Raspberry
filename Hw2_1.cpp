87103c8252e4cd1bc0448805b940a0a13d26811f
YJK <114245163+Y-jin126@users.noreply.github.com>
2024-04-01 21:42:53 +0900

    Update Hw2_1.cpp

--- Hw2_1.cpp
+++ Hw2_1.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <wiringPi.h>
+#include <unistd.h>
+using namespace std;
+
+#define LED_GPIO      17      // this is GPIO17, Pin 11
+#define BUTTON_GPIO   27      // this is GPIO27, Pin 13
+#define DEBOUNCE_TIME 200
+#define stateLED 0
+
+
+void lightLED(void){
+  static unsigned long lastISRTime = 0,  x = 1;
+  unsigned long currentISRTime = millis();
+  if(currentISRTime - lastISRTime > DEBOUNCE_TIME){
+    digitalWrite(LED_GPIO, !stateLED);         // turn the LED on
+    cout << "Button pressed " << x++ << " times! LED on" << endl;
+  }
+  lastISRTime = currentISRTime;
+}
+
+int main() {                             // must be run as root
+  wiringPiSetupGpio();                  // use the GPIO numbering
+  pinMode(LED_GPIO, OUTPUT);            // the LED
+  pinMode(BUTTON_GPIO, INPUT);          // the Button
+  digitalWrite (LED_GPIO, LOW);         // LED is off
+  cout << "Press the button on GPIO " << BUTTON_GPIO << endl;
+  // call the lightLED() ISR on the rising edge (i.e., button press)
+  wiringPiISR(BUTTON_GPIO, INT_EDGE_RISING, &lightLED);
+  for(int i=10; i>0; i--){              // countdown to program end
+    cout << "You have " << i << " seconds remaining..." << endl;
+    sleep(1);                          // sleep for 1 second
+  }
+  return 0;                             // program ends after 10s
+}
 
