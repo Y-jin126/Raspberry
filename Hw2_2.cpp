0daf47c0e33b0b731664cc0525c70192de86f7b4
YJK <114245163+Y-jin126@users.noreply.github.com>
2024-04-02 16:05:15 +0900

    Update Hw2_2.cpp

--- Hw2_2.cpp
+++ Hw2_2.cpp
@@ -2,30 +2,30 @@
 #include <wiringPi.h>
 #include <unistd.h>
 using namespace std;
-#define PWM_RLED   25
-#define PWM_GLED  23
+#define PWM_LED1   13
+#define PWM_LED2  18
 
 int main() {
   wiringPiSetupGpio();
-  pinMode(PWM_RLED, PWM_OUTPUT);
-  pinMode(PWM_GLED, PWM_OUTPUT);
+  pinMode(PWM_LED1, PWM_OUTPUT);
+  pinMode(PWM_LED2, PWM_OUTPUT);
   pwmSetRange(1000);
 
 while(true) {
   for(int i= 1; i < 1000; i++){
-    pwmWrite(PWM_RLED,i);
+    pwmWrite(PWM_LED1,i);
     usleep(1000);
   }
   for(int i = 999; i>=0; i--){
-    pwmWrite(PWM_RLED, i);
+    pwmWrite(PWM_LED1, i);
     usleep(1000);
     }
   for(int i= 1; i < 1000; i++){
-    pwmWrite(PWM_GLED,i);
+    pwmWrite(PWM_LED2,i);
     usleep(1000);
   }
   for(int i = 999; i>=0; i--){
-    pwmWrite(PWM_GLED, i);
+    pwmWrite(PWM_LED2, i);
     usleep(1000);
     }
   
