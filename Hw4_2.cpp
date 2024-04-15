840ade17d0c2860e8a0f1e06cbdf4229b6bf99e3
YJK <114245163+Y-jin126@users.noreply.github.com>
2024-04-15 20:17:58 +0900

    Update Hw4_2.cpp

--- Hw4_2.cpp
+++ Hw4_2.cpp
@@ -1,1 +1,29 @@
-#include
+#include <stdio.h>
+#include <wiringPi.h>
+#include <mcp3004.h>
+#define BASE 200
+#define SPI_CHAN 0
+
+#define RLED_GPIO 23
+#define GLED_GPIO 24
+#define BLED_GPIO 21
+
+int main(void) {
+ wiringPiSetup();
+  mcp3004Setup(BASE, SPI_CHAN); 
+  pinMode(RLED_GPIO, OUTPUT);
+  pinMode(GLED_GPIO, OUTPUT);
+  pinMode(BLED_GPIO, OUTPUT);
+ 
+ int x;
+ 
+ while (1) {
+ x = analogRead(BASE);
+ if (x < 500)
+ printf("%d\n", x);
+ delay(100);
+
+   
+ }
+ return 0;
+}
