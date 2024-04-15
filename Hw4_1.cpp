8bf1c0c533fab3153ce751c4a611fd92d28c0bde
YJK <114245163+Y-jin126@users.noreply.github.com>
2024-04-15 18:00:43 +0900

    Update Hw4_1.cpp

--- Hw4_1.cpp
+++ Hw4_1.cpp
@@ -6,6 +6,9 @@
 #include <wiringPi.h>
 #include <iomanip>
 #include <unistd.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+
 using namespace std;
 // Small macro to display value in hexadecimal with 2 places
 #define HEX(x) setw(2) << setfill('0') << hex << (int)(x)
